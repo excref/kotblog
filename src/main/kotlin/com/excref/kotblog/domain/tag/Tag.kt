c1e62a67f74903d9d2845ddd7eeb4d64515402db
Arthur Asatryan <biacoder@gmail.com>
2017-06-04 20:20:09 +0400

    #4 Added tag domain model created, updated and removed properties

--- src/main/kotlin/com/excref/kotblog/domain/tag/Tag.kt
+++ src/main/kotlin/com/excref/kotblog/domain/tag/Tag.kt
@@ -1,6 +1,7 @@
 package com.excref.kotblog.domain.tag
 
 import java.io.Serializable
+import java.time.LocalDateTime
 import java.util.*
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
@@ -12,8 +13,11 @@ import javax.persistence.Id
  */
 @Entity
 data class Tag(
-        val name: String = "",
+        val name: String,
         val uuid: String = UUID.randomUUID().toString(),
+        val created: LocalDateTime = LocalDateTime.now(),
+        val updated: LocalDateTime = LocalDateTime.now(),
+        val removed: LocalDateTime? = null,
         @Id
         @GeneratedValue val id: Long? = null
 ) : Serializable
