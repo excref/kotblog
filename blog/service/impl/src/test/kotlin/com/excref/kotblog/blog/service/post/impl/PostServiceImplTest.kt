e0dcd7389811e08809d1725b06d4cccd630240a6
rafayel <rafayel.mirzoyan@sflpro.com>
2017-06-11 16:16:08 +0400

    [#27] Tests are corrected. All is working fine.

--- blog/service/impl/src/test/kotlin/com/excref/kotblog/blog/service/post/impl/PostServiceImplTest.kt
+++ blog/service/impl/src/test/kotlin/com/excref/kotblog/blog/service/post/impl/PostServiceImplTest.kt
@@ -4,18 +4,19 @@ import com.excref.kotblog.blog.persistence.post.PostRepository
 import com.excref.kotblog.blog.service.blog.BlogService
 import com.excref.kotblog.blog.service.category.CategoryService
 import com.excref.kotblog.blog.service.category.domain.Category
+import com.excref.kotblog.blog.service.category.exception.PostNotExistsForUuidException
 import com.excref.kotblog.blog.service.post.PostService
 import com.excref.kotblog.blog.service.post.domain.Post
 import com.excref.kotblog.blog.service.tag.TagService
 import com.excref.kotblog.blog.service.tag.domain.Tag
 import com.excref.kotblog.blog.service.test.AbstractServiceImplTest
 import org.assertj.core.api.Assertions.assertThat
-import org.easymock.EasyMock
 import org.easymock.EasyMock.expect
 import org.easymock.EasyMock.isA
 import org.easymock.Mock
 import org.easymock.TestSubject
 import org.junit.Test
+import java.util.*
 
 /**
  * @author Rafayel Mirzoyan
@@ -52,6 +53,47 @@ class PostServiceImplTest : AbstractServiceImplTest(){
     }
     //endregion
 
+    //region getByUuid
+
+    /**
+     *  when nothing
+     */
+    @Test
+    fun testGetByUuid() {
+        resetAll()
+        // test data
+        val uuid = UUID.randomUUID().toString()
+        // expectations
+        expect(postRepository.findByUuid(uuid)).andReturn(null)
+        replayAll()
+        // test scenario
+        try {
+            postService.getByUuid(uuid)
+        } catch (ex: PostNotExistsForUuidException) {
+            assertThat(ex).isNotNull().extracting("uuid").containsOnly(uuid)
+        }
+        verifyAll()
+    }
+
+    /**
+     * main scenario
+     */
+    @Test
+    fun getByUuid2() {
+        resetAll()
+        // test data
+        val post = helper.buildPost()
+        val uuid = post.uuid
+        // expectations
+        expect(postRepository.findByUuid(uuid)).andReturn(post)
+        replayAll()
+        // test scenario
+        val result = postService.getByUuid(uuid)
+        assertThat(result).isNotNull().isEqualTo(post)
+        verifyAll()
+    }
+    //endregion
+
     //region create
 
     /**
@@ -59,7 +101,7 @@ class PostServiceImplTest : AbstractServiceImplTest(){
      */
 
     @Test
-    fun create2() {
+    fun create() {
         resetAll()
         val post: Post = helper. buildPost()
         val tagUuids = post.tags.map { tg: Tag -> tg.uuid }.toList()
@@ -69,10 +111,12 @@ class PostServiceImplTest : AbstractServiceImplTest(){
         expect(blogService.getByUuid(post.blog.uuid)).andReturn(helper.buildBlog())
         expect(tagService.getByUuids(tagUuids)).andReturn(listOf(helper.buildTag()))
         expect(categoryService.getByUuids(categoryUuids)).andReturn(listOf(helper.buildCategory()))
-        expect(postRepository.save(isA(Post::class.java))).andAnswer({ EasyMock.getCurrentArguments()[0] as Post? })
+        expect(postRepository.save(isA(Post::class.java))).andReturn(post)
         replayAll()
         // test scenario
-        val category = postService.create(post.name, post.title, post.content, post.blog.uuid, tagUuids, categoryUuids)
+        val result = postService.create(post.name, post.title, post.content, post.blog.uuid, tagUuids, categoryUuids)
+        assertThat(result).isNotNull().isEqualTo(post)
+
         verifyAll()
     }
     //endregion
